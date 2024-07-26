352210a26999a745d418f30ad2426325a5390940
Janine Wagner <janine.vvagner@gmail.com>
2024-07-26 17:35:07 +0200

    generate dev-wallet

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use solana_sdk::{signature::{Keypair, Signer}, pubkey::Pubkey};
+
+    #[test]
+    fn keygen(){
+        let kp = Keypair::new();
+        println!("You've generated a new Solana wallet: {}", kp.pubkey().to_string());
+        println!("");
+        println!("To save your wallet, copy and paste the following into a JSON file:");
+        println!("{:?}", kp.to_bytes());
+    }
+    #[test]
+    fn airdrop(){}
+    #[test]
+    fn transfer_sol(){}
+}
